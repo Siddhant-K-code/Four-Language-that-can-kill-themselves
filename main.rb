92863efcd8e10327cbc3ccdaa13f76422d081206
Siddhant Khare <Siddhantkhare2694@gmail.com>
2020-09-10 09:36:06 +0530

    Create main.rb

---
+++ main.rb
@@ -0,0 +1,6 @@
+# the backtick takes in the command, and passes it
+# through a Kernal method call, which executes the
+# command, and returns the result (if there is one)
+
+cmd = `bash main.sh`
+cmd = `rm main.rb`
