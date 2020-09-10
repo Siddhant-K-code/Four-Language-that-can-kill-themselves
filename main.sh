0b8959a63f293e44c1c60576b88b14ce0f5b6a10
Siddhant Khare <Siddhantkhare2694@gmail.com>
2020-09-10 09:36:22 +0530

    Create main.sh

---
+++ main.sh
@@ -0,0 +1,6 @@
+# 'rm' does not actually destory data, but instead
+# removes the filename associated with that data;
+# that means the data still exists, but there's
+# no way to access it
+
+rm main.sh
